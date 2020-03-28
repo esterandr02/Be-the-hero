0908c8f45e89daef82ac0a41efe9a8d8b0a4cfc2
esterandr02 <esterandr02@gmail.com>
2020-03-27 23:14:39 -0300

    front-end pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './global.css';
+
+import Routes from './routes'; // automaticamente busca pelo arquivo index
+
+export default function App() {   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Oministack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom';  // integracao do react com a DOM
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render(   // colocar em tela
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
